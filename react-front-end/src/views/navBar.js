f08a073c8433181b8afba7f7776f2a953b585274
Graeme Chalmers <chalmers.graeme@gmail.com>
2020-11-22 17:57:05 -0800

    Added navbar functionality

--- react-front-end/src/views/navBar.js
+++ react-front-end/src/views/navBar.js
@@ -1,14 +1,40 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText
+} from 'reactstrap';
 
-function NavBar() {
-  return(
-    <ul>
-      <li><Link to='/'>Home</Link></li>
-      <li><Link to='/login'>Login</Link></li>
-      <li><Link to='/register'>Register</Link></li>
-    </ul>
-  )
+
+const NavBar = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="dark" dark expand="md">
+        <NavbarBrand href="/">GigFinder</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/login">Login</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/register">Register</NavLink>
+            </NavItem>
+          </Nav>
+          <NavbarText>Corporate Tinder!</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
 }
 
 export default NavBar;
