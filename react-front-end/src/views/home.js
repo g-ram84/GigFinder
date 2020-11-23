a97e40e46516e3135a33aedb8dce071529ef8071
Graeme Chalmers <chalmers.graeme@gmail.com>
2020-11-22 19:58:04 -0800

    Updated search bar with button.

--- react-front-end/src/views/home.js
+++ react-front-end/src/views/home.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SearchBar from '../components/searchBar.js';
+import Button from '../components/button.js'
+import '../styles/homePage.scss';
 
 function Home(props) {
+  const [input, setInput] = useState('');
   return (
     <div>
+      <span class="title">
       <h1>Welcome to GigFinder!</h1>
+      <SearchBar
+      />
+      </span>
     </div>
   );
 }
