d97dda48cce6887e531e32eb135ba8054687e7fc
ebussiere <eddy@embtech.ca>
2020-11-25 00:44:12 +0000

    Search bar option

--- react-front-end/src/components/jobList.js
+++ react-front-end/src/components/jobList.js
@@ -1,15 +1,14 @@
 import React, { useContext } from "react";
 import JobListItem from "./jobListItem.js";
 
-import "./jobListItem.scss"
+import "./jobListItem.scss";
 import GigfinderContext from '../context/gigfinder/gigfinderContext.js';
 
 export default function JobList(props) {
   const gigfinderContext = useContext(GigfinderContext);
   const { jobs } = gigfinderContext;
-  const filteredJobList = jobs.filter((job) => job.job_title.toLowerCase().includes(props.query.toLowerCase()))
 
-  const myJobs = filteredJobList.map(job => {
+  const myJobs = jobs.map(job => {
     const date = new Date(job.job_date);
     const year = date.getFullYear();
     let month = date.getMonth() + 1;
@@ -19,35 +18,35 @@ export default function JobList(props) {
       dt = '0' + dt;
     }
     if (month < 10) {
-      month = '0' + month
+      month = '0' + month;
     }
 
-    const startDate = `${year}-${month}-${dt}`
+    const startDate = `${year}-${month}-${dt}`;
 
 
     return (
       <div>
-      <JobListItem
-        key={job.id}
-        title={job.job_title}
-        wage={job.hourly_wage}
-        date={job.date_created}
-        jobDate={startDate}
-        active={job.active}
-        positions={job.positions}
-        description={job.job_description}
-        company={job.employer_id}
-        location={job.location_lat, job.location_long}
-      />
+        <JobListItem
+          key={job.id}
+          title={job.job_title}
+          wage={job.hourly_wage}
+          date={job.date_created}
+          jobDate={startDate}
+          active={job.active}
+          positions={job.positions}
+          description={job.job_description}
+          company={job.employer_id}
+          location={job.location_lat, job.location_long}
+        />
       </div>
     );
-   
-    
+
+
   });
   return (
     <section>
       <span className="myJob">{myJobs}</span>
     </section>
   );
- 
+
 }
