2dcd91e73dc3ed46c4f137b19fb1e9aa50445443
Graeme Chalmers <chalmers.graeme@gmail.com>
2020-11-25 13:16:04 -0800

    updated

--- react-front-end/src/components/jobListItem.js
+++ react-front-end/src/components/jobListItem.js
@@ -1,13 +1,7 @@
 import React from "react";
-<<<<<<< HEAD
-import "./jobListItem.scss"
-import { Router, Route, useHistory, Link } from 'react-router-dom';
-import Results from '../views/results.js'
-=======
 import "./jobListItem.scss";
 import { Link } from 'react-router-dom';
 import Results from '../views/results.js';
->>>>>>> master
 
 const classNames = require('classnames');
 
@@ -16,10 +10,6 @@ export default function JobListItem(props) {
   return (
     <div className="jobs_item">
       <Link className="job_header" exact to={`/${props.jobId}`}>
-<<<<<<< HEAD
-=======
-        {/* <Link className=“job_header” exact to={`/${props.jobId}`}> */}
->>>>>>> master
         <strong>{props.title}</strong>
       </Link>
       <br />
