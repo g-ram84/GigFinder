4a2d64bb54d29ba9019ffb7885fcc2114376d39b
Graeme Chalmers <chalmers.graeme@gmail.com>
2020-11-26 12:09:19 -0800

    added external links

--- react-front-end/src/components/apply.js
+++ react-front-end/src/components/apply.js
@@ -7,18 +7,37 @@ import "./jobListItem.scss"
 import { render } from 'react-dom';
 
 
-export default function Apply() {
+export default function Apply(props) {
   const gigfinderContext = useContext(GigfinderContext);
   const { loggedInUser } = gigfinderContext;
+  const date = new Date(Date.now());
+  const year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let dt = date.getDate();
+  if (dt < 10) {
+    dt = '0' + dt;
+  }
+  if (month < 10) {
+    month = '0' + month;
+  }
+  const applyDate = `${year}-${month}-${dt}`
 
+  const newApp = {
+    worker_id: loggedInUser.id,
+    job_id: props.jobID,
+    status: 'Pending',
+    date_applied: applyDate
+  };
+  console.log("props", props)
   const onSubmit = e => {
     e.preventDefault();
-    gigfinderContext.addNewApplication(loggedInUser);
+    gigfinderContext.addNewApplication(newApp);
   }
+  console.log("aplication", newApp)
   return (
     <div>
       <Form onSubmit={onSubmit}>
-      <Button>Apply Now!</Button>
+        <Button>Apply Now!</Button>
       </Form>
     </div>
   )
