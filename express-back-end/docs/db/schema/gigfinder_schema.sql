efdd2142cfbe573f7120a9f09b653ace6f693460
ebussiere <eddy@embtech.ca>
2020-11-26 21:18:55 +0000

    new db

---
+++ express-back-end/docs/db/schema/gigfinder_schema.sql
@@ -0,0 +1,67 @@
+DROP TABLE IF EXISTS saved_employers CASCADE;
+DROP TABLE IF EXISTS saved_workers CASCADE;
+DROP TABLE IF EXISTS applications CASCADE;
+DROP TABLE IF EXISTS jobs CASCADE;
+DROP TABLE IF EXISTS workers CASCADE;
+DROP TABLE IF EXISTS employers CASCADE;
+
+CREATE TABLE workers (
+	id SERIAL PRIMARY KEY NOT NULL,
+	first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  phone VARCHAR(12) NOT NULL,
+  avatar_url VARCHAR(255),
+  sin VARCHAR(12),
+  resume_upload VARCHAR(255)
+);
+
+CREATE TABLE employers (
+	id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  contact_name VARCHAR(50) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  phone_number VARCHAR(12) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  website VARCHAR(255) NOT NULL,
+  description VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE jobs (
+	id SERIAL PRIMARY KEY NOT NULL,
+	job_title VARCHAR(50),
+	hourly_wage INTEGER,
+	job_date DATE,
+	job_active BOOLEAN NOT NULL,
+	positions INTEGER DEFAULT 1,
+	job_description VARCHAR(255) DEFAULT 'Job Description',
+	employer_id INTEGER REFERENCES employers(id),
+	job_location VARCHAR(255) DEFAULT 'Job Location'
+);
+
+CREATE TABLE saved_employers (
+	id SERIAL PRIMARY KEY NOT NULL,
+	worker_id INTEGER REFERENCES workers(id),
+	employer_id INTEGER REFERENCES employers(id),
+	favourite BOOLEAN DEFAULT FALSE,
+	rating INTEGER DEFAULT 0
+);
+
+CREATE TABLE saved_workers (
+	id SERIAL PRIMARY KEY NOT NULL,
+	worker_id INTEGER REFERENCES workers(id),
+	employer_id INTEGER REFERENCES employers(id),
+	favourite BOOLEAN DEFAULT FALSE,
+	rating INTEGER DEFAULT 0
+);
+
+CREATE TABLE applications (
+	id SERIAL PRIMARY KEY NOT NULL,
+	worker_id INTEGER REFERENCES workers(id),
+	job_id INTEGER REFERENCES Jobs(id),
+	status VARCHAR(20) DEFAULT 'Pending',
+	date_applied DATE NOT NULL
+);
+
+
+
---
+++ express-back-end/docs/db/seeds/gigfinder_seeds.sql
@@ -0,0 +1,62 @@
+--WORKERS SEEDS
+INSERT INTO workers (first_name, last_name, email, phone, avatar_url, sin, resume_upload) VALUES ('Sonny', 'Sowrey', 'ssowrey0@infoseek.co.jp', '604-782-5426', 'https://robohash.org/rationemolestiasnobis.jpg?size=50x50&set=set1', '453-77-6007', 'resume.pdf');
+INSERT INTO workers (first_name, last_name, email, phone, avatar_url, sin, resume_upload) VALUES ('Norah', 'Andryszczak', 'nandryszczak1@mac.com', '604-084-4659', 'https://robohash.org/vitaereiciendisvoluptatem.jpg?size=50x50&set=set1', '825-34-7068', 'resume.pdf');
+INSERT INTO workers (first_name, last_name, email, phone, avatar_url, sin, resume_upload) VALUES ('Nicholle', 'Casero', 'ncasero2@lulu.com', '604-676-4048', 'https://robohash.org/estetin.png?size=50x50&set=set1', '783-86-7149', 'resume.pdf');
+INSERT INTO workers (first_name, last_name, email, phone, avatar_url, sin, resume_upload) VALUES ('Jandy', 'Tunnock', 'jtunnock3@reverbnation.com', '604-771-9880', 'https://robohash.org/eligendietut.jpg?size=50x50&set=set1', '331-46-7660', 'resume.pdf');
+INSERT INTO workers (first_name, last_name, email, phone, avatar_url, sin, resume_upload) VALUES ('Edith', 'Lauder', 'elauder4@sina.com.cn', '604-312-9340', 'https://robohash.org/aliasvitaeratione.bmp?size=50x50&set=set1', '800-74-7843', 'resume.pdf');
+INSERT INTO workers (first_name, last_name, email, phone, avatar_url, sin, resume_upload) VALUES ('Patty', 'Assur', 'passur5@mapquest.com', '604-246-7567', 'https://robohash.org/sitiustodoloribus.jpg?size=50x50&set=set1', '174-89-7977', 'resume.pdf');
+INSERT INTO workers (first_name, last_name, email, phone, avatar_url, sin, resume_upload) VALUES ('Karoly', 'Waudby', 'kwaudby6@zdnet.com', '604-099-4014', 'https://robohash.org/rerumnonreprehenderit.png?size=50x50&set=set1', '366-78-5151', 'resume.pdf');
+
+--EMPLOYERS SEEDS
+INSERT INTO employers (name, contact_name, email, phone_number, password, website, description) VALUES ('Jayo','Paul Biford','pbiford0@nymag.com','604-697-1346','5escAkj0JgO8','reuters.com','natoque penatibus et magnis dis parturient montes nascetur ridiculus mus etiam vel augue vestibulum rutrum rutrum neque aenean auctor gravida');
+INSERT INTO employers (name, contact_name, email, phone_number, password, website, description) VALUES ('Dabjam','Lindsey Whitby','lwhitby1@free.fr','604-029-7470','Wd5XVqhbi','ucoz.com','est congue elementum in hac habitasse platea dictumst morbi vestibulum velit id');
+INSERT INTO employers (name, contact_name, email, phone_number, password, website, description) VALUES ('Minyx','Dorri Oliveira','doliveira3@shutterfly.com','604-672-7068','0LWsmk6lW2ZW','clickbank.net','mi pede malesuada in imperdiet et commodo vulputate justo in blandit ultrices enim lorem ipsum');
+INSERT INTO employers (name, contact_name, email, phone_number, password, website, description) VALUES ('Voonix','Waldemar Arnott','warnott2@zdnet.com','604-034-2635','X7esVKPgg8','wikimedia.org','fusce congue diam id ornare imperdiet sapien urna pretium nisl ut');
+INSERT INTO employers (name, contact_name, email, phone_number, password, website, description) VALUES ('Kayveo','Sig Honeyghan','shoneyghan4@w3.org','604-197-8730','0yNvPl','nhs.uk','ac leo pellentesque ultrices mattis odio donec vitae nisi nam ultrices libero non mattis pulvinar');
+INSERT INTO employers (name, contact_name, email, phone_number, password, website, description) VALUES ('Zoonder','Susie Creamer','screamer5@angelfire.com','604-738-8754','wFPBrtSQqL','apple.com','sapien iaculis congue vivamus metus arcu adipiscing molestie hendrerit at vulputate vitae nisl aenean');
+INSERT INTO employers (name, contact_name, email, phone_number, password, website, description) VALUES ('Roomm','Tallou Oake','toake6@google.de','604-200-6222','RTf8Q6llvsC','craigslist.org','porttitor pede justo eu massa donec dapibus duis at velit eu est congue elementum in hac');
+INSERT INTO employers (name, contact_name, email, phone_number, password, website, description) VALUES ('Blogtags','Philippine Python','ppython7@pbs.org','604-217-8636','Vgn9nG6GP','prnewswire.com','varius ut blandit non interdum in ante vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia');
+
+--JOBS SEEDS
+INSERT INTO jobs (job_title, hourly_wage, job_date, job_active, positions, job_description, employer_id, job_location) VALUES ('musician', 29, '2020/12/02', true, 2, 'purus eu magna vulputate luctus cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus vivamus vestibulum sagittis sapien cum sociis natoque', 2, 'Location 2');
+INSERT INTO jobs (job_title, hourly_wage, job_date, job_active, positions, job_description, employer_id, job_location) VALUES ('musician', 17, '2020/12/01', true, 1, 'neque aenean auctor gravida sem praesent id massa id nisl venenatis lacinia aenean sit amet justo morbi ut odio cras mi pede malesuada in imperdiet et commodo vulputate', 1, 'Location 1');
+INSERT INTO jobs (job_title, hourly_wage, job_date, job_active, positions, job_description, employer_id, job_location) VALUES ('labourer', 26, '2020/12/02', true, 3, 'felis fusce posuere felis sed lacus morbi sem mauris laoreet ut rhoncus aliquet pulvinar sed nisl nunc rhoncus dui vel sem sed sagittis nam congue risus semper porta', 3, 'Location 3');
+INSERT INTO jobs (job_title, hourly_wage, job_date, job_active, positions, job_description, employer_id, job_location) VALUES ('handyman', 17, '2020/12/02', true, 2, 'vel nulla eget eros elementum pellentesque quisque porta volutpat erat quisque erat eros viverra eget congue eget semper rutrum nulla nunc purus phasellus in felis', 4, 'Location 4');
+INSERT INTO jobs (job_title, hourly_wage, job_date, job_active, positions, job_description, employer_id, job_location) VALUES ('musician', 20, '2020/12/01', true, 1, 'morbi sem mauris laoreet ut rhoncus aliquet pulvinar sed nisl nunc rhoncus dui vel sem sed sagittis nam congue risus semper porta volutpat quam pede lobortis ligula', 5, 'Location 5');
+INSERT INTO jobs (job_title, hourly_wage, job_date, job_active, positions, job_description, employer_id, job_location) VALUES ('labourer', 16, '2020/12/01', true, 2, 'vehicula condimentum curabitur in libero ut massa volutpat convallis morbi odio odio elementum eu interdum eu tincidunt in leo maecenas pulvinar lobortis est phasellus', 6, 'Location 6');
+INSERT INTO jobs (job_title, hourly_wage, job_date, job_active, positions, job_description, employer_id, job_location) VALUES ('cleaner', 25, '2020/12/01', true, 2, 'dictumst maecenas ut massa quis augue luctus tincidunt nulla mollis molestie lorem quisque', 7, 'Location 7');
+INSERT INTO jobs (job_title, hourly_wage, job_date, job_active, positions, job_description, employer_id, job_location) VALUES ('labourer', 26, '2020/12/02', true, 1, 'id ornare imperdiet sapien urna pretium nisl ut volutpat sapien arcu sed augue aliquam erat volutpat in congue etiam justo etiam pretium iaculis justo in hac habitasse', 8, 'Location 8');
+INSERT INTO jobs (job_title, hourly_wage, job_date, job_active, positions, job_description, employer_id, job_location) VALUES ('busser', 28, '2020/12/02', true, 1, 'nunc vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae mauris viverra diam vitae quam suspendisse potenti nullam porttitor lacus at', 1, 'Location 9');
+INSERT INTO jobs (job_title, hourly_wage, job_date, job_active, positions, job_description, employer_id, job_location) VALUES ('painter', 21, '2020/12/01', true, 2, 'sapien iaculis congue vivamus metus arcu adipiscing molestie hendrerit at vulputate vitae nisl aenean lectus pellentesque eget nunc donec quis orci eget orci vehicula', 2, 'Location 10');
+
+--APPLICATIONS SEEDS
+INSERT INTO applications (worker_id, job_id, status, date_applied) VALUES (1, 1, 'Pending', '12/2/2020');
+INSERT INTO applications (worker_id, job_id, status, date_applied) VALUES (2, 2, 'Pending', '12/1/2020');
+INSERT INTO applications (worker_id, job_id, status, date_applied) VALUES (3, 3, 'Declined', '12/1/2020');
+INSERT INTO applications (worker_id, job_id, status, date_applied) VALUES (4, 3, 'Accepted', '12/2/2020');
+INSERT INTO applications (worker_id, job_id, status, date_applied) VALUES (5, 5, 'Pending', '12/2/2020');
+INSERT INTO applications (worker_id, job_id, status, date_applied) VALUES (6, 6, 'Accepted', '12/1/2020');
+INSERT INTO applications (worker_id, job_id, status, date_applied) VALUES (7, 7, 'Pending', '12/2/2020');
+INSERT INTO applications (worker_id, job_id, status, date_applied) VALUES (1, 8, 'Pending', '12/1/2020');
+INSERT INTO applications (worker_id, job_id, status, date_applied) VALUES (2, 9, 'Accepted', '12/2/2020');
+INSERT INTO applications (worker_id, job_id, status, date_applied) VALUES (3, 10, 'Accepted', '12/2/2020');
+INSERT INTO applications (worker_id, job_id, status, date_applied) VALUES (4, 1, 'Declined', '12/2/2020');
+INSERT INTO applications (worker_id, job_id, status, date_applied) VALUES (5, 2, 'Declined', '12/1/2020');
+
+--SAVED EMPLOYERS SEEDS
+INSERT INTO saved_employers (worker_id, employer_id, favourite, rating) VALUES (1, 2, false, 5);
+INSERT INTO saved_employers (worker_id, employer_id, favourite, rating) VALUES (2, 7, true, 2);
+INSERT INTO saved_employers (worker_id, employer_id, favourite, rating) VALUES (3, 8, false, 2);
+INSERT INTO saved_employers (worker_id, employer_id, favourite, rating) VALUES (4, 7, true, 1);
+INSERT INTO saved_employers (worker_id, employer_id, favourite, rating) VALUES (5, 6, true, 4);
+INSERT INTO saved_employers (worker_id, employer_id, favourite, rating) VALUES (6, 6, false, 3);
+INSERT INTO saved_employers (worker_id, employer_id, favourite, rating) VALUES (7, 3, true, 4);
+
+--SAVED WORKER SEEDS
+INSERT INTO saved_workers (worker_id, employer_id, favourite, rating) VALUES (1, 2, false, 5);
+INSERT INTO saved_workers (worker_id, employer_id, favourite, rating) VALUES (2, 7, true, 2);
+INSERT INTO saved_workers (worker_id, employer_id, favourite, rating) VALUES (3, 8, false, 2);
+INSERT INTO saved_workers (worker_id, employer_id, favourite, rating) VALUES (4, 7, true, 1);
+INSERT INTO saved_workers (worker_id, employer_id, favourite, rating) VALUES (5, 6, true, 4);
+INSERT INTO saved_workers (worker_id, employer_id, favourite, rating) VALUES (6, 6, false, 3);
+INSERT INTO saved_workers (worker_id, employer_id, favourite, rating) VALUES (7, 3, true, 4);
