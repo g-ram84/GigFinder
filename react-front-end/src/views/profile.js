b9948d53f455b98c4c4bf7a60be1c8ea629a652c
ebussiere <eddy@embtech.ca>
2020-11-26 17:40:48 +0000

    remove pdf package

--- react-front-end/src/views/profile.js
+++ react-front-end/src/views/profile.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import PDFViewer from 'pdf-viewer-reactjs';
+
 
 
 
 function Profile() {
   return (
-    //     <PDFViewer
-    //     document={{
-    //         url: 'https://arxiv.org/pdf/quant-ph/0410100.pdf',
-    //     }}
-    // />
+
     <div>
       <h1>Profile Page.  Swipe Right!</h1>
     </div>
