a095646d63d489fb98bc6dc26730a7d711177d3a
Graeme Chalmers <chalmers.graeme@gmail.com>
2020-11-26 16:34:33 -0800

    got rid of console.log

--- react-front-end/src/components/jobList.js
+++ react-front-end/src/components/jobList.js
@@ -3,7 +3,7 @@ import JobListItem from "./jobListItem.js";
 import "./jobListItem.scss";
 import GigfinderContext from '../context/gigfinder/gigfinderContext.js';
 
-export default function JobList() {
+export default function JobList(props) {
   const gigfinderContext = useContext(GigfinderContext);
   const { jobs } = gigfinderContext;
   const myJobs = jobs.map(job => {
@@ -18,7 +18,7 @@ export default function JobList() {
       month = '0' + month;
     }
     const startDate = `${year}-${month}-${dt}`;
-    console.log("job", job)
+    
     return (
       <div>
         <JobListItem
@@ -31,7 +31,6 @@ export default function JobList() {
           positions={job.positions}
           description={job.job_description}
           company={job.employer_id}
-          location={job.location_lat, job.location_long}
         />
       </div>
     );
