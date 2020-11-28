8c57663a1570b81429df36fc4f8eb9db3be59abf
Eddy <eddy@embtech.ca>
2020-11-28 12:09:22 -0700

    Redo Context 2

--- react-front-end/src/context/types.js
+++ react-front-end/src/context/types.js
@@ -1,7 +1,7 @@
-export const SEARCH_JOBS = 'SEARCH_JOBS';
 export const GET_FAVE_EMPLOYERS = 'GET_FAVE_EMPLOYERS';
 export const GET_FAVE_WORKERS = 'GET_FAVE_WORKERS';
 export const SEARCH_WORKERS = 'SEARCH_WORKERS';
+export const SEARCH_JOBS = 'SEARCH_JOBS';
 export const ADD_JOB = 'ADD_JOB';
 export const CLEAR_JOBS = 'CLEAR_JOBS';
 export const LOG_IN_WORKER = 'LOG_IN_WORKER';
