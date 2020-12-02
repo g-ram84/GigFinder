f57fe806d728bf86d165409e21f24b49eac37e7a
ebussiere <eddy@embtech.ca>
2020-12-02 18:32:00 +0000

    Add resumes

--- express-back-end/routes/smsRoutes.js
+++ express-back-end/routes/smsRoutes.js
@@ -1,8 +1,9 @@
 const { app } = require("../server");
 function smsRoutes(client) {
   app.post("/sms/applicationdenied", (req, res) => {
+    console.log("Denied");
     client.messages.create({
-      from: "+15873168138",
+      from: "+12283358040",
       to: "+14037019333",
       body: "Your application was not succesful"
     }).then((message) => {
@@ -11,9 +12,9 @@ function smsRoutes(client) {
   });
 
   app.post("/sms/applicationaccepted", (req, res) => {
-    console.log("Here");
+    console.log("Accepted");
     client.messages.create({
-      from: "+15873168138",
+      from: "+12283358040",
       to: "+14037019333",
       body: "You'hired!!"
     }).then((message) => {
